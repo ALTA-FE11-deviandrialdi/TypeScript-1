de981acd3c5ffb6a2ca1ade2eb6f83e5889a5f30
Devi Andri Aldi <deviandrialdi@gmail.com>
2022-12-07 01:02:50 +0700

    TypeScript1

--- Part1/problem1/problem1.ts
+++ Part1/problem1/problem1.ts
@@ -1,3 +1,11 @@
 export const acronym = (phrase: string) => {
   // your code here
+
+  let array: Array<string> = [];
+  let words: Array<string> = phrase.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    array.push(words[i][0].toUpperCase());
+  }
+
+  return array.join("");
 };
